33cb2e128d55018f1c6fe4471e873676e26c6f86
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-12 18:25:18 +0200

    Added SupportedArgumentTypes.cs, also for testing

---
+++ src/TypedScripts/TypedScripts.Tests/Arguments/SupportedArgumentTypes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TypedScripts.Tests.Arguments;
+
+public class SupportedArgumentTypes : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        foreach (var type in TypedScripts.Arguments.SupportedArgumentTypes.All)
+        {
+            yield return [type];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
