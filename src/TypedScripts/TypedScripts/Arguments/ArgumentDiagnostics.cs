47626096abe1b2031bc103af24cb9ef38d9bcce2
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-12 20:39:13 +0200

    Added ArgumentDiagnostics.cs

---
+++ src/TypedScripts/TypedScripts/Arguments/ArgumentDiagnostics.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using TypedScripts.Arguments.Exceptions;
+
+namespace TypedScripts.Arguments;
+
+public static class ArgumentDiagnostics
+{
+    public static DiagnosticDescriptor UnsupportedArgumentType(UnsupportedArgumentTypeException ex) => new(
+        id: "TSARG001",
+        title: "Unsupported argument type",
+        messageFormat: ex.Message,
+        category: "TypedScripts",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+    
+    public static DiagnosticDescriptor InvalidArgumentDefault(InvalidArgumentDefaultException ex) => new(
+        id: "TSARG002",
+        title: "Invalid default value",
+        messageFormat: ex.Message,
+        category: "TypedScripts",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+    
+    
+    public static DiagnosticDescriptor UnsupportedArgumentDefault(UnsupportedArgumentDefaultException ex) => new(
+        id: "TSARG003",
+        title: "Unsupported argument default",
+        messageFormat: ex.Message,
+        category: "TypedScripts",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+}
