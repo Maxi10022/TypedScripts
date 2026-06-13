ca34a7b8fe050098c6e114db708fc940698c7507
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-13 23:30:25 +0200

    Added Script related exceptions

---
+++ src/TypedScripts/TypedScripts.Generator/Scripts/Exceptions/InvalidScriptSyntaxException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace TypedScripts.Scripts.Exceptions;
+
+public class InvalidScriptSyntaxException(Diagnostic[] diagnostics) : Exception
+{
+    public Diagnostic[] Diagnostics { get; } = diagnostics;
+}
