7584eeebbd7cf6da57100b25d47cfbf702aea6a7
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-13 23:26:51 +0200

    Added Shell.cs enum

---
+++ src/TypedScripts.Core/Shell.cs
@@ -0,0 +1,18 @@
+﻿namespace TypedScripts.Core;
+
+/// <summary>
+/// List of supported shells - extensible in the future.
+/// </summary>
+public enum Shell
+{
+    Bash
+}
+
+public static class ShellExtensions
+{
+    public static string GetShellName(this Shell shell) => shell switch
+    {
+        Shell.Bash => "Bash",
+        _ => "Undefined"
+    };
+}
