ea7b889a3f67bd6ee5a8ecb2a7a603cddeb284bc
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-17 16:54:45 +0200

    Reanmed old source ScriptGenerator.cs to IncrementalScriptGenerator.cs, refactored and simplified implementation

---
+++ src/TypedScripts.Generator/IncrementalScriptGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using Microsoft.CodeAnalysis;
+using TypedScripts.Scripts;
+using TypedScripts.Scripts.Detection;
+using TypedScripts.Scripts.Generation;
+
+namespace TypedScripts;
+
+[Generator]
+public class IncrementalScriptGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var scriptGenerator = new ScriptGenerator();
+        var scriptCandidates = context.AdditionalTextsProvider.ComposeScriptCandidatesProvider();
+        context.RegisterSourceOutput(scriptCandidates, scriptGenerator.Generate);
+    }
+}
