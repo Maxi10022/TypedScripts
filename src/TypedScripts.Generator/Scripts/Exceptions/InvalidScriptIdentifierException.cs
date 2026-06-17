b07b8ef176cff61bb314c73539939d665b0a1b15
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-17 15:25:46 +0200

    Removed InvalidScriptIdentifierException.cs, replaced by InvalidIdentifierException.cs

--- src/TypedScripts.Generator/Scripts/Exceptions/InvalidScriptIdentifierException.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace TypedScripts.Scripts.Exceptions;
-
-public class InvalidScriptIdentifierException(string name) 
-    : Exception($"Script name '{name}' is not a valid C# identifier.");
