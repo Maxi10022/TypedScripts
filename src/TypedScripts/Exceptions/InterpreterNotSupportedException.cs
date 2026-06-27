6aca6698624064e30e2b72ce52ed4bc8b7283ec9
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-06-27 18:29:08 +0200

    Added InterpreterNotSupportedException.cs used by generated scripts for validation purposes

---
+++ src/TypedScripts/Exceptions/InterpreterNotSupportedException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace TypedScripts.Exceptions;
+
+public class InterpreterNotSupportedException(Interpreter interpreter, string scriptName) 
+    : Exception($"Interpreter '{interpreter.GetName()}' is not supported by {scriptName}");
