fdb878e89600078d3c464082c9a63390705a73d3
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-07-25 23:30:51 +0200

    Added SshExecutionOutput.cs, works as output forwarder for SSH command executions

---
+++ src/TypedScripts/Output/SshExecutionOutput.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace TypedScripts.Output;
+
+public class SshExecutionOutput(SshCommand cmd, Task execTask) : IExecutionOutput
+{
+    public void Dispose() => cmd.Dispose();
+
+    public Stream StandardOutput => cmd.OutputStream;
+    public Stream StandardError => cmd.ExtendedOutputStream;
+    
+    public async Task<int> WaitForExitAsync()
+    {
+        var result = cmd.BeginExecute();
+        await execTask.ConfigureAwait(false);
+        return cmd.ExitStatus ?? -1;
+    }
+}
