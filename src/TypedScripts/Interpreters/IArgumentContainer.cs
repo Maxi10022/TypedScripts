83eededfe31f4541c821ddc30385fec467d33407
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-07-25 23:21:59 +0200

    Added IArgumentContainer.cs, used for code-generated argument objects.

---
+++ src/TypedScripts/Interpreters/IArgumentContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace TypedScripts.Interpreters;
+
+/// <summary>
+/// Used for code-generated argument DTO objects.
+/// </summary>
+public interface IArgumentContainer
+{
+    /// <summary>
+    /// Convert the DTO to an argument string for the script to execute.
+    /// </summary>
+    /// <param name="formatter">Argument formatter for the scripts' interpreter.</param>
+    /// <returns>Single-line argument string passed to the script.</returns>
+    public string ToArgumentString(ArgumentFormatter formatter);
+}
