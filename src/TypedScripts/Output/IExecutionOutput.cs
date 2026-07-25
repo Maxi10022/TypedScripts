1cf00be56417e1467b05c7722f567b98c14c2d48
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-07-25 23:26:45 +0200

    Added IExecutionOutput.cs interface for getting a script executions output.

---
+++ src/TypedScripts/Output/IExecutionOutput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TypedScripts.Output;
+
+/// <summary>
+/// Object containing streams callers can read/pull data from.
+/// </summary>
+public interface IExecutionOutput : IDisposable
+{
+    /// <summary>
+    /// The standard-out stream.
+    /// </summary>
+    public Stream StandardOutput { get; }
+    
+    /// <summary>
+    /// The standard-error stream.
+    /// </summary>
+    public Stream StandardError { get; }
+    
+    /// <summary>
+    /// Wait for the scripts execution to complete.
+    /// <b>Cancellation token passed when <c>Execute</c> was called cancels the task!</b> 
+    /// </summary>
+    /// <returns>The scripts exit code.</returns>
+    public Task<int> WaitForExitAsync(); 
+}
