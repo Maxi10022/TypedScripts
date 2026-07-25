d2efb8a8638ea1b0e069e7bf4e56770a24b955c7
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-07-25 23:40:25 +0200

    Added IExecutor.cs, used as template for executor implementations

---
+++ src/TypedScripts/Executors/IExecutor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TypedScripts.Output;
+
+namespace TypedScripts.Executors;
+
+/// <summary>
+/// Interface used to implement interpreter executions.
+/// A single interpreter might have more than one implementation,
+/// if they target different execution targets (e.g. SSH vs Local).
+/// </summary>
+public interface IExecutor
+{
+    /// <summary>
+    /// The <see cref="Interpreter"/> this executor supports, used for validation in <see cref="ExecuteAsync"/>. 
+    /// </summary>
+    public Interpreter Handles { get; }
+    
+    /// <summary>
+    /// Executes the script and returns a streamed response.
+    /// </summary>
+    /// <param name="options">Options carrying script data.</param>
+    /// <param name="ct">Cancellation token used to cancel the async <see cref="IExecutionOutput"/>.</param>
+    /// <returns>Instance of <see cref="IExecutionOutput"/>.</returns>
+    public Task<IExecutionOutput> ExecuteAsync(ExecutionOptions options, CancellationToken ct = default);
+}
