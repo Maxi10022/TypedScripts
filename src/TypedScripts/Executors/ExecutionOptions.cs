a398af36b0801ffaa685c14b724994020a4f8a92
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-07-25 23:36:04 +0200

    Added ExecutionOptions.cs, provides script content, its arguments and the target interpreter

---
+++ src/TypedScripts/Executors/ExecutionOptions.cs
@@ -0,0 +1,28 @@
+﻿using TypedScripts.Interpreters;
+
+namespace TypedScripts.Executors;
+
+/// <summary>
+/// Usually constructed by the code-generated script object. 
+/// </summary>
+/// <param name="script">The actual script content to execute.</param>
+/// <param name="interpreter">The interpreter to use.</param>
+/// <param name="arguments">An argument container required to support a method to build the arg string.</param>
+public class ExecutionOptions(string script, Interpreter interpreter, IArgumentContainer arguments)
+{
+    /// <summary>
+    /// The actual script to execute.
+    /// </summary>
+    public string Script { get; } = script;
+    
+    /// <summary>
+    /// The shell to use to execute that script.
+    /// </summary>
+    public Interpreter Interpreter { get; } = interpreter;
+    
+    /// <summary>
+    /// Ordered list of script arguments, order must be preserved.
+    /// This means the first item must be the first argument etc.
+    /// </summary>
+    public IArgumentContainer Arguments { get; } = arguments;
+}
