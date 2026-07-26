8a53c7e70ede6740c68eaa46b6f8b655fc7158d7
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2026-07-26 14:21:05 +0200

    Added RequiredParameterUnsetException.cs

---
+++ src/TypedScripts/Exceptions/RequiredParameterUnsetException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace TypedScripts.Exceptions;
+
+public class RequiredParameterUnsetException(string argName, string argType) 
+    : Exception($"Argument of type '{argType}' has unset required parameter '{argName}'.");
